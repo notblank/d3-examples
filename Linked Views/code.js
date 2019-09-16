529780f822e1a550cf92720f73146c5e0a3e9fcf
FZ <fz@MacBook-Pro-de-FZ.local>
2019-09-16 16:16:22 -0500

    Linked Views

--- Linked Views/code.js
+++ Linked Views/code.js
@@ -1,63 +1,110 @@
-let body = d3.select("#body");
-let container = d3.select("#container");
+let barchart = d3.select("#barchart");
+let timeline = d3.select("#timeline");
 
-d3.csv("./sat.csv")
-  .then(sat => showData(sat));
+let width = 600;
+let height = 320;
 
-function showTooltip(text, coords) {
-  d3.select('#tooltip')
-    .text(text)
-    .style('top', coords[1] + "px")
-    .style('left', coords[0] + "px")
-    .style('display', 'block');
+d3.csv("./unemploymentG7.csv")
+  .then((data) => {
+    data = prepareData(data);
+    drawBarchart(data);
+  });
+
+let selectedCountry = undefined;
+
+function prepareData(data){
+  return data.map(d => {
+    // tries to convert to number, if it cant returns NaN:
+    let years = Object.keys(d).filter(k => !isNaN(+k));
+    let history = years.map(function (y) {
+      return {year: +y, value: +d[y], Country: d.Country};
+    });
+    d.History = history;
+    return d;
+  })
 }
 
-function showData(sat) {
+function drawLineChart(data){
 
-  let maxParticipation = d3.max(sat, d => +d.Participation);
-  let maxTotal = d3.max(sat, d => +d.Total);
+  data = data.History;
 
-  let partScale = d3.scaleLinear()
-    .domain([0, maxParticipation])
-    .range([50, 250]);
+  let xScale = d3.scaleLinear()
+                  .domain(d3.extent(data, d => d.year))
+                  .range([0, width - 20]);
 
-  let totalScale = d3.scaleLinear()
-    .domain([maxTotal, 1200])
-    .range([50, 250]);
+  let yScale = d3.scaleLinear()
+                  .domain([0, d3.max(data, d => d.value)])
+                  .range([height, 0]);
 
   // Axes:
-  let xAxis = d3.axisBottom(partScale);
-  let yAxis = d3.axisLeft(totalScale);
+  let xAxis = d3.axisBottom(xScale);
+  let yAxis = d3.axisLeft(yScale);
 
-  d3.select('#xAxis')
+  timeline.select('.xAxis')
+          .call(xAxis)
+          .attr('transform', 'translate(20, 330)');
+
+  timeline.select('.yAxis')
+          .call(yAxis)
+          .attr('transform', 'translate(20, 0)');
+
+  let lineGen = d3.line()
+                  .x(d => xScale(d.year))
+                  .y(d => yScale(d.value));
+
+  timeline.select('.body')
+          .append('path')
+          .datum(data)
+          .attr('d', lineGen)
+          .attr('transform', 'translate(-130, 0)')
+          .attr('class', 'line')
+          .attr('stroke', 'grey');
+          
+}
+
+function drawBarchart(data){
+
+  let xScale = d3.scaleBand()
+                  .domain(data.map(d => d.Country))
+                  .range([0, width - 20])
+                  .padding(0.1);
+
+  let yScale = d3.scaleLinear()
+                  .domain([0, d3.max(data, d => +d[2018])])
+                  .range([height, 0]);
+
+  // Axes:
+  let xAxis = d3.axisBottom(xScale);
+  let yAxis = d3.axisLeft(yScale);
+
+  d3.select('.xAxis')
     .call(xAxis)
-    .attr('transform', 'translate(150, 250)');
+    .attr('transform', 'translate(20, 330)');
 
-  d3.select('#yAxis')
+  d3.select('.yAxis')
     .call(yAxis)
-    .attr('transform', 'translate(180, 0)');
+    .attr('transform', 'translate(20, 0)');
 
-  let join = body.selectAll('circle')
-    .data(sat);
+  let barchartBody = barchart.select('.body')
+                             .selectAll('rect')
+                             .data(data);
 
-  join.enter()
-    .append('circle')
-    .style('fill', 'blue')
-    .attr('r', '5')
-    .attr('cx', d => partScale(d.Participation))
-    .attr('cy', d => totalScale(d.Total))
-    .on('mouseover', d => {
-      let coords = [d3.event.clientX, d3.event.clientY];
-      showTooltip(d.State, coords);
-    })
-    // to show while on the object
-    // .on('mousemove', d => {
-    //   let coords = [d3.event.clientX, d3.event.clientY];
-    //   showTooltip(d.State, coords);
-    // })
-    .on('mouseleave', d => {
-      d3.select('#tooltip')
-        .style('display', 'none')
-    });
+  barchartBody.enter()
+              .append('rect')
+              .attr('fill', 'grey')
+              .attr('x', d => xScale(d.Country))
+              .attr('y', d => yScale(+d[2018]))
+              .attr('width', xScale.bandwidth())
+              .attr('height',d => height - yScale(+d[2018]))
+              .attr('transform', 'translate(-130, 0)')
+              .on('click', d => {
+                // remove previous line:
+                timeline.selectAll('.line').remove();
+                selectedCountry = d.Country;
+                drawBarchart(data);
+                drawLineChart(d);
+              })
+              .merge(barchartBody)
+              .attr('fill', d => selectedCountry === d.Country ? 'red' : 'grey');
 
 }
